a240f520ae3e4dd2613607b5d21beb8f06fbe646
Roberto Machorro <7190436+RobertoMachorro@users.noreply.github.com>
2022-01-25 18:33:58 -0500

    Implemented CLI arguments for application.

--- webxtract/main.swift
+++ webxtract/main.swift
@@ -8,4 +8,30 @@
 import Foundation
 import ArgumentParser
 
-print("Hello, World!")
+struct Webxtract: ParsableCommand {
+	@Flag(help: "Include debugging information.")
+	var verbose = false
+
+	@Option(name: .shortAndLong, help: "The number of times to attempt page download.")
+	var attempts: Int?
+
+	@Argument(help: "The source's web address.")
+	var url: String
+
+	@Argument(help: "A Regular Expression to apply on content.")
+	var expression: String
+
+	mutating func run() throws {
+		let attemptsCount = attempts ?? 1
+
+		for i in 1...attemptsCount {
+			if verbose {
+				print("\(url) verbose (attempt \(i))")
+			} else {
+				print(url)
+			}
+		}
+	}
+}
+
+Webxtract.main()
